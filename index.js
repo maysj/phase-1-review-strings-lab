8a7a727dec769ad199f9a62cd3d5dfa46dd9297b
Mays Jones <maysjones2@gmail.com>
2022-03-15 10:25:20 -0400

    commit tree c322e426cc60c586e93ba70789fc6dad335e0454

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'user one'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+const upperCaseCurrentUser = `USER ONE`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+let shortGreeting ='Welcome, u!'
